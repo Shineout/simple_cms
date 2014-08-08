c2c5fc9c2af98ffba0c601db202c91f362beeb6d
Oleg Krasnov <mail@olegkrasnov.com>
2014-08-08 21:08:58 +0400

    Сделал задачи по миграции

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,24 @@
 class DemoController < ApplicationController
+
+	layout false
+
   def index
+
+
+  end
+
+  def hello
+
+  	@array = [1,2,3,4,55]
+
+  	# @id = params [:id]
+  	# @page = params [:page]
+
+
+
+  end
+
+  def other_hello
+  	redirect_to ("http://www.ya.ru")
   end
 end
